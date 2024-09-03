80c38de9472dd34fd7c41ca3590cdb727c4c50c1
lucasfigerno <164235144+lucasfigerno@users.noreply.github.com>
2024-09-03 10:51:10 -0400

    Commit Aula 7 parte 2
    Commit Aula 7 parte 2

---
+++ exercicio desafio 3.py
@@ -0,0 +1,8 @@
+class Livro:
+    def __init__(self, titulo, autor, ano_publicacao, situacao):
+        self.titulo = titulo
+        self.autor = autor
+        self.ano_publicacao = ano_publicacao
+        self.situacao = situacao
+
+
