90845d07c15721ebe46ad8c34b850c64fcc35b34
Alexandre Francisco de Almeida <alexandre.francisco.almeida2024@hotmail.com>
2024-09-05 09:14:21 -0400

    CLIENT-SIDECommit Final do Curso!!
    CLIENT-SIDE

--- exercicio desafio 3.py
+++ exercicio desafio 3.py
@@ -4,5 +4,25 @@ class Livro:
         self.autor = autor
         self.ano_publicacao = ano_publicacao
         self.situacao = situacao
+        
+    def mostrar(self):
+        print(a)
+        print(b)
+        print(c)
+        print(d)
 
+a = input("Informe Titulo do Livro : ")
+b = input("Informe Autor do Livro : ")
+c = input("Informe Ano do Livro : ")
+d = input("Informe Situacao do Livro : ")
 
+emprestar = mostrar()
+devolver = Livro("teste", "teste", 1971, "B")
+
+class Biblioteca:
+    def __init__(self, livros):
+        self.livros = livros
+
+adicionarlivro = Biblioteca("teste")
+emprestar = Biblioteca("teste")
+devolver = Biblioteca("teste")
